7fcb09e001391cacff80f5b3470150b8381e1c61
yumeyuniko <daigocafe@msn.com>
2022-03-24 22:07:48 +0900

    movie detail

---
+++ pages/contact.js
@@ -0,0 +1,9 @@
+const contact = () => {
+  return (
+    <div className="text-center mt-20">
+      <h1 className="text-4xl font-bold">Contact Us</h1>
+    </div>
+  );
+};
+
+export default contact;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,27 @@
-export default function Home() {
+import axios from 'axios';
+import Hero from '../components/Hero';
+import PopularMovie from '../components/PopularMovie';
+import { server } from '../config';
+
+export default function Home({ movies }) {
+  console.log(movies);
   return (
-    <div>
-      <h1 className="text-2xl text-center font-bold">Home</h1>
+    <div className="bg-gray-700">
+      <Hero />
+      <PopularMovie movies={movies.results} />
     </div>
   );
 }
+
+export async function getStaticProps() {
+  const res = await axios(
+    `${server}/popular?api_key=${process.env.API_KEY}&language=ja-JP&page=1`
+  );
+  const movies = res.data;
+  return {
+    props: { movies },
+  };
+}
+
+//https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1
+//https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=ja&page=1
